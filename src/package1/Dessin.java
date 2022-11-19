8218d69dc9fa61a3fc55d314600a27fcfb13ebf9
pitchou15 <real-abdou23@outlook.fr>
2022-11-19 22:49:18 +0100

    aj-class-dessin

--- src/package1/Dessin.java
+++ src/package1/Dessin.java
@@ -11,7 +11,7 @@ public class Dessin extends JPanel{
 	/**
      * stocke la liste des trains ajout�es à cette zone de dessin.
      */
-    private final List<TrainCercle> listeTrain = new CopyOnWriteArrayList();
+    private final List<IObjetDessinable> objetDessin = new CopyOnWriteArrayList();
 
     /**
      * retourne la largeur de la zone de dessin.
@@ -39,10 +39,10 @@ public class Dessin extends JPanel{
      */
     public void ajouterObjet(TrainCercle t) {
 
-        if (!listeTrain.contains(t)) {
+        if (!objetDessin.contains(t)) {
             // l'objet n'est pas déjà dans la liste
             // on le rajoute a la liste des objets du dessin
-        	listeTrain.add(t);
+        	objetDessin.add(t);
             // le dessin se réaffiche
             repaint();
             this.pause(10);
@@ -70,7 +70,7 @@ public class Dessin extends JPanel{
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         //  dessiner les trains que contient le dessin
-        for (TrainCercle c : listeTrain) {
+        for (IObjetDessinable c : objetDessin) {
             c.dessiner(g);
         }
     }
@@ -81,7 +81,7 @@ public class Dessin extends JPanel{
      */
     public void animer() {
         //  dessiner les Objets que contient le dessin
-        for (TrainCercle c : listeTrain) {
+        for (IObjetDessinable c : objetDessin) {
         	c.deplacer();
             
         }
